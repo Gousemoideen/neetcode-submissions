37cd5db108a092cfa7602a11f39816b77b6cd896
Gouse Moideen <gousemoideen1@gmail.com>
2026-06-11 12:11:01 +0530

    Add: find-duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+    int slow = 0, fast = 0;
+    
+    // Phase 1: detect cycle
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+    
+    // Phase 2: find entry point
+    slow = 0;
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
+};
